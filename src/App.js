035b918fed0cd9a4b4b52db05f71d792c57ce86c
Asim <asim005011997@gmail.com>
2021-05-20 16:48:47 +0530

    Credit card using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import Cards from 'react-credit-cards'
+import DatePicker from 'react-datepicker'
+import 'react-credit-cards/es/styles-compiled.css'
+import 'react-datepicker/dist/react-datepicker.css'
 import './App.css';
 
 function App() {
+  const [cNumber, setNumber] = useState('')
+  const [cName, setName] = useState('')
+  const [cExpiry, setExpiry] = useState('')
+  const [cCvv, setCvv] = useState('')
+  const [cFocus, setFocus] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards className="Card" number={cNumber} name={cName} expiry={cExpiry} cvc={cCvv} focused={cFocus} />
+      <div className="divStyle">
+      <form>
+        <label style={{margin:"10px", float:"left"}}>Card Number</label>
+        <input type='number' name='number' placeholder='Card Number' value={cNumber} onChange={ e => setNumber(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+        <br/>
+
+        <label style={{margin:"10px", float:"left"}}>Card Name</label>
+        <input type='text' name='name' placeholder='Name' value={cName} onChange={ e => setName(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+        <br/>
+        <label style={{margin:"10px", float:"left"}}>Expiration Date</label>
+        <input type='tel' name='expiry' placeholder='MM/YY Expiry' value={cExpiry} onChange={ e => setExpiry(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+        <label style={{margin:"10px", float:"left"}}>CVV</label>
+        <input  type='password' name='cvc' placeholder='CVC' value={cCvv} onChange={ e => setCvv(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+        <button className="Button">Submit</button>
+      </form>
+      </div>
     </div>
   );
 }
