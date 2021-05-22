059294ca59603cdee4b29ae985f8a9190e68e625
Asim <asim005011997@gmail.com>
2021-05-22 11:48:02 +0530

    Modifying the inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
 import Cards from 'react-credit-cards'
-import DatePicker from 'react-datepicker'
 import 'react-credit-cards/es/styles-compiled.css'
-import 'react-datepicker/dist/react-datepicker.css'
 import './App.css';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure()
 
 function App() {
   const [cNumber, setNumber] = useState('')
@@ -12,23 +14,35 @@ function App() {
   const [cCvv, setCvv] = useState('')
   const [cFocus, setFocus] = useState('')
 
+
   return (
     <div className="App">
       <Cards className="Card" number={cNumber} name={cName} expiry={cExpiry} cvc={cCvv} focused={cFocus} />
       <div className="divStyle">
       <form>
         <label style={{margin:"10px", float:"left"}}>Card Number</label>
-        <input type='number' name='number' maxlength='16' placeholder='Card Number' value={cNumber} onChange={ e => setNumber(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+        <input type='number' name='number' maxLength='16' placeholder='Card Number' value={cNumber} onChange={ e => setNumber(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
         <br/>
 
         <label style={{margin:"10px", float:"left"}}>Card Name</label>
         <input type='text' name='name' placeholder='Name' value={cName} onChange={ e => setName(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
         <br/>
-        <label style={{margin:"10px", float:"left"}}>Expiration Date</label>
-        <input type='tel' name='expiry' maxlength='6' placeholder='MM/YY Expiry' value={cExpiry} onChange={ e => setExpiry(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
-        <label style={{margin:"10px", float:"left"}}>CVV</label>
-        <input  type='password' name='cvc' maxlength='4' placeholder='CVC' value={cCvv} onChange={ e => setCvv(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
-        <button className="Button">Submit</button>
+        <table>
+        <tr>
+          <td style={{width:"70%"}}>
+            <label style={{margin:"10px", float:"left"}}>Expiration Date</label>
+            <input style={{width:"90%"}} type='tel' name='expiry' maxLength='7' placeholder='MM/YY Expiry' value={cExpiry} onChange={ e => setExpiry(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+          </td>
+          <td>
+            <label style={{margin:"10px", float:"left"}}>CVV</label>
+           <input  type='password' name='cvc' maxLength='4' placeholder='CVC' value={cCvv} onChange={ e => setCvv(e.target.value)} onFocus={ e => setFocus(e.target.name)} />
+           </td>
+          </tr>
+      </table>
+        <button className="Button" onClick={()=>{
+         toast('Submitted') 
+         console.log("Submit")
+        }}>Submit</button>
       </form>
       </div>
     </div>
